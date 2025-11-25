464811fdbd60da9f1f53240663b73aff3a0d2662
YuliaSemak <yuliia.a.semak@ukd.edu.ua>
2025-11-25 16:00:43 +0200

    Create replace.js

---
+++ replace.js
@@ -0,0 +1,11 @@
+const fs = require(`fs`)
+
+const data = fs.readFileSync(
+    'book.txt',
+    {
+        encoding: 'utf8',
+        flag: 'r'
+    }
+);
+
+console.log(data.replaceAll(" ", "(Semak Julia)"));
